0edc2695d2eda144a51f329498337d6444628eba
xiao.xinmiao <xiaoxm_001@163.com>
2018-01-08 10:18:33 +0800

    api:ipaytype for admin

--- controllers/ipay_type.go
+++ controllers/ipay_type.go
@@ -1,1 +1,131 @@
 package controllers
+
+import (
+	"bmapping-api/factory"
+	"bmapping-api/models"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
+)
+
+type IPayTypeApiController struct {
+}
+
+func (c IPayTypeApiController) Init(g *echo.Group) {
+	g.GET("", c.GetAll)
+	g.POST("", c.Create)
+	g.GET("/:id", c.GetOne)
+	g.PUT("/:id", c.Update)
+}
+
+func (e IPayTypeApiController) Create(c echo.Context) error {
+	status := c.QueryParam("status")
+	switch status {
+	case "batch":
+		return e.InsertMany(c)
+	default:
+		return e.InsertOne(c)
+	}
+}
+
+func (IPayTypeApiController) InsertMany(c echo.Context) error {
+	v := make([]models.IpayType, 0)
+	if err := c.Bind(&v); err != nil {
+		return ReturnApiFail(c, http.StatusBadRequest, ApiErrorParameter, err)
+	}
+	if err := (models.IpayType{}).InsertMany(c.Request().Context(), &v); err != nil {
+		return ReturnApiFail(c, http.StatusInternalServerError, ApiErrorDB, err)
+	}
+
+	return ReturnApiSucc(c, http.StatusOK, make([]interface{}, 0))
+}
+
+func (IPayTypeApiController) GetAll(c echo.Context) error {
+	var v SearchInput
+	if err := c.Bind(&v); err != nil {
+		return ReturnApiFail(c, http.StatusBadRequest, ApiErrorParameter, err)
+	}
+	if v.MaxResultCount == 0 {
+		v.MaxResultCount = DefaultMaxResultCount
+	}
+
+	factory.BehaviorLogger(c.Request().Context()).WithBizAttr("maxResultCount", v.MaxResultCount).Log("SearchElandStore")
+
+	factory.Logger(c.Request().Context()).WithFields(logrus.Fields{
+		"sortby":         v.Sortby,
+		"order":          v.Order,
+		"maxResultCount": v.MaxResultCount,
+		"skipCount":      v.SkipCount,
+	}).Info("SearchInput")
+
+	totalCount, items, err := models.IpayType{}.GetAll(c.Request().Context(), v.Sortby, v.Order, v.SkipCount, v.MaxResultCount)
+	if err != nil {
+		return ReturnApiFail(c, http.StatusInternalServerError, ApiErrorDB, err)
+	}
+
+	factory.BehaviorLogger(c.Request().Context()).
+		WithCallURLInfo(http.MethodGet, "https://play.google.com/books", nil, 200).
+		WithBizAttrs(map[string]interface{}{
+			"totalCount": totalCount,
+			"itemCount":  len(items),
+		}).
+		Log("SearchComplete")
+
+	return ReturnApiSucc(c, http.StatusOK, ArrayResult{
+		TotalCount: totalCount,
+		Items:      items,
+	})
+}
+
+func (IPayTypeApiController) InsertOne(c echo.Context) error {
+	var v models.IpayType
+	if err := c.Bind(&v); err != nil {
+		return ReturnApiFail(c, http.StatusBadRequest, ApiErrorParameter, err)
+	}
+	if err := c.Validate(&v); err != nil {
+		return ReturnApiFail(c, http.StatusBadRequest, ApiErrorParameter, err)
+	}
+	if err := (&v).InsertOne(c.Request().Context()); err != nil {
+		return ReturnApiFail(c, http.StatusInternalServerError, ApiErrorDB, err)
+	}
+
+	return ReturnApiSucc(c, http.StatusOK, v)
+}
+
+func (IPayTypeApiController) GetOne(c echo.Context) error {
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		return ReturnApiFail(c, http.StatusBadRequest, ApiErrorParameter, err)
+	}
+	has, v, err := models.IpayType{}.GetById(c.Request().Context(), id)
+	if err != nil {
+		return ReturnApiFail(c, http.StatusInternalServerError, ApiErrorDB, err)
+	}
+	if !has {
+		return ReturnApiFail(c, http.StatusNotFound, ApiErrorNotFound, nil)
+	}
+
+	return ReturnApiSucc(c, http.StatusOK, v)
+}
+
+func (IPayTypeApiController) Update(c echo.Context) error {
+	var v models.IpayType
+	if err := c.Bind(&v); err != nil {
+		return ReturnApiFail(c, http.StatusBadRequest, ApiErrorParameter, err)
+	}
+	if err := c.Validate(&v); err != nil {
+		return ReturnApiFail(c, http.StatusBadRequest, ApiErrorParameter, err)
+	}
+
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		return ReturnApiFail(c, http.StatusBadRequest, ApiErrorParameter, err)
+	}
+	if err := (&v).Update(c.Request().Context(), id); err != nil {
+		return ReturnApiFail(c, http.StatusInternalServerError, ApiErrorDB, err)
+	}
+
+	return ReturnApiSucc(c, http.StatusOK, v)
+}
--- models/ipay_type.go
+++ models/ipay_type.go
@@ -1,6 +1,93 @@
 package models
 
+import (
+	"bmapping-api/factory"
+	"context"
+	"errors"
+
+	"github.com/go-xorm/xorm"
+)
+
 type IpayType struct {
 	Id          int64
 	Description string
+}
+
+func (IpayType) InsertMany(ctx context.Context, ipayTypes *[]IpayType) (err error) {
+	row, err := factory.DB(ctx).Insert(ipayTypes)
+	if int(row) == 0 {
+		err = errors.New("no data has changed.")
+		return
+	}
+	return
+}
+
+func (c *IpayType) InsertOne(ctx context.Context) (err error) {
+	row, err := factory.DB(ctx).InsertOne(c)
+	if int(row) == 0 {
+		err = errors.New("no data has changed.")
+		return
+	}
+	return
+}
+
+func (IpayType) GetById(ctx context.Context, id int64) (has bool, ipayType *IpayType, err error) {
+	ipayType = &IpayType{}
+	has, err = factory.DB(ctx).ID(id).Get(ipayType)
+	return
+}
+
+func (IpayType) GetAll(ctx context.Context, sortby, order []string, offset, limit int) (totalCount int64, items []IpayType, err error) {
+	queryBuilder := func() *xorm.Session {
+		q := factory.DB(ctx)
+		if err := setSortOrder(q, sortby, order); err != nil {
+			factory.Logger(ctx).Error(err)
+		}
+		return q
+	}
+
+	errc := make(chan error)
+	go func() {
+		v, err := queryBuilder().Count(&IpayType{})
+		if err != nil {
+			errc <- err
+			return
+		}
+		totalCount = v
+		errc <- nil
+
+	}()
+
+	go func() {
+		if err := queryBuilder().Limit(limit, offset).Find(&items); err != nil {
+			errc <- err
+			return
+		}
+		errc <- nil
+	}()
+
+	if err := <-errc; err != nil {
+		return 0, nil, err
+	}
+	if err := <-errc; err != nil {
+		return 0, nil, err
+	}
+	return
+}
+func (c *IpayType) Update(ctx context.Context, id int64) (err error) {
+	row, err := factory.DB(ctx).ID(id).Update(c)
+	if int(row) == 0 {
+		err = errors.New("no data has changed.")
+		return
+	}
+	return
+}
+
+func (IpayType) Delete(ctx context.Context, id int64) (err error) {
+	row, err := factory.DB(ctx).ID(id).Delete(&IpayType{})
+	if int(row) == 0 {
+		err = errors.New("no data has changed.")
+		return
+	}
+	return
 }
